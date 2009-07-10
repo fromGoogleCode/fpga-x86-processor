cbdc6b8e0c1444cfd238dac5070bc76a3d7ebdf4
leonardoaraujo.santos <leonardoaraujo.santos@a31241fa-699c-11de-ba0c-230037762ff9>
2009-07-10 02:49:45 +0000

    More working

--- alu.vhd
+++ alu.vhd
@@ -7,11 +7,13 @@ Use IEEE.Std_Logic_1164.all;
 
 entity ALU is port
    (
-     x                       : in    std_logic_vector(31 downto 0);
+     x                       : in    std_logic_vector(15 downto 0);
      y                       : in    std_logic_vector(15 downto 0);
-     output                  : out   std_logic_vector(31 downto 0);
-     flags                   : out   std_logic_vector(7 downto 0);
-     DEN                     : out   std_logic
+     output                  : out   std_logic_vector(15 downto 0);
+     operation               : in    std_logic_vector (6 DOWNTO 0);
+     reset                   : in    std_logic;
+     clk                     : in    std_logic;
+     flags                   : out   std_logic_vector(7 downto 0)
    );
 end ALU;
 --------------------------------------------------------------------------------
